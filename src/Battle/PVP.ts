d47583f36c4f94f33ce06e08506d4a4b06ede665
Felipe Duarte <feduarte.dev@gmail.com>
2024-01-25 19:47:21 -0300

    req 11 pronto

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -2,21 +2,21 @@ import Fighter from '../Fighter';
 import Battle from './Battle';
 
 class PVP extends Battle {
-  private player1: Fighter;
-  private player2: Fighter;
+  private _player1: Fighter;
+  private _player2: Fighter;
 
   constructor(player1: Fighter, player2: Fighter) {
     super(player1);
 
-    this.player1 = player1;
-    this.player2 = player2;
+    this._player1 = player1;
+    this._player2 = player2;
   }
 
   override fight(): number {
-    this.player1.attack(this.player2);
-    this.player2.attack(this.player1);
+    this._player1.attack(this._player2);
+    this._player2.attack(this._player1);
 
-    return this.player1.lifePoints <= 0 || this.player2.lifePoints <= 0
+    return this._player1.lifePoints <= 0 || this._player2.lifePoints <= 0
       ? super.fight() : this.fight();
   }
 }
